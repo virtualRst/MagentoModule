a599e0fa8ad2d6c6a25befbf7b4d77c2fac395fa
virtualrst <rishabhtiwari821@gmail.com>
2022-04-01 14:18:59 +0530

    Wroking in part2 of US3

---
+++ Observer/Log2.php
@@ -0,0 +1,44 @@
+<?php
+/**
+*
+* Copyright © Magento. All rights reserved.
+* See COPYING.txt for license details.
+*/
+namespace Hummingbird\Module\Observer;
+/**
+* Class Log
+*/
+class Log2 implements \Magento\Framework\Event\ObserverInterface
+{
+/**
+* @var \Psr\Log\LoggerInterface
+*/
+private $_logger;
+/**
+* @var \Magento\Framework\App\RequestInterface
+*/
+private $_request;
+/**
+* Log constructor.
+* @param \Psr\Log\LoggerInterface $logger
+* @param \Magento\Framework\App\RequestInterface $request
+*/
+public function __construct(
+\Psr\Log\LoggerInterface $logger,
+\Magento\Framework\App\RequestInterface $request)
+{
+$this->_logger = $logger;
+$this->_request = $request;
+}
+/**
+* @param \Magento\Framework\Event\Observer $observer
+*/
+public function execute(\Magento\Framework\Event\Observer $observer)
+{
+    $product = $observer->getProduct();
+    $originalName = $product->getName();
+    $modifiedName = $originalName . ' - Modified by Magento 2 Events and Observers';
+    $product->setName($modifiedName);
+    $this->_logger->info($modifiedName);
+}
+}
